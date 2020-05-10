56f3b1e6fab1b64882c13bab1dd3774807f09005
dgonzalezg <dmgonzalezg@uc.cl>
2020-05-10 16:10:35 -0400

    Updates

---
+++ precache-manifest.df8ed89663848e97232e3ffdcf86bba5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d04446f0a4f96264ef6310f8ae31c761",
+    "url": "/t3dgonzalezg/index.html"
+  },
+  {
+    "revision": "c013c643cc3b623460b5",
+    "url": "/t3dgonzalezg/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fac9bcd3903d2d329dc1",
+    "url": "/t3dgonzalezg/static/js/2.835ea3ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/t3dgonzalezg/static/js/2.835ea3ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c013c643cc3b623460b5",
+    "url": "/t3dgonzalezg/static/js/main.224b0760.chunk.js"
+  },
+  {
+    "revision": "05825cbd6480c97b3e26",
+    "url": "/t3dgonzalezg/static/js/runtime-main.8154f64b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/t3dgonzalezg/static/media/logo.5d5d9eef.svg"
+  }
+]);
